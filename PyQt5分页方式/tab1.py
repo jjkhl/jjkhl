fb5f717c3321e8927311df113db2a70ce4488a52
unknown <877798492@qq..com>
2021-10-06 00:36:05 +0800

    PyQt5分页方式

--- PyQt5分页方式/tab1.py
+++ PyQt5分页方式/tab1.py
@@ -4,17 +4,16 @@ Version: 2.0
 Autor: jjkhl
 Date: 2021-10-05 23:36:56
 LastEditors: jjkhl
-LastEditTime: 2021-10-05 23:59:42
+LastEditTime: 2021-10-06 00:33:40
 '''
 from PyQt5.QtCore import *
 
 class Tab1:
     def __init__(self,mainwindow):
       mainwindow.pushButton.clicked.connect(self.push)
-    #    self.window_object=window_object
-    #    self.lineEdit=window_object.lineEdit
-    
+      self.mainwindow=mainwindow
     def push(self):
         # self.lineEdit.setText("按钮按下")
         # window_object.lineEdit("按钮按下")
-        print("按钮按下")
+        # print("按钮按下")
+        self.mainwindow.lineEdit.setText("按钮按下")
